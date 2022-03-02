04999a37180de71279eb8cecac6b589bd050a5fc
Alexia Velador <alexiavel01@gmail.com>
2022-03-02 09:44:57 -0700

    commit tree 7fb74670fd6a272af746efe712cae1167579fd35

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  document.querySelector('p').textContent = "This is really cool!";
+});
+
+//document.getElementById('text').textContent = 'This is really cool!';
+
+
+  //   document.querySelector("text").addEventListener('submit', (e) => {
+  //     e.preventDefault()
+  //     buildToDo(e.target.new_todo.value)
+  //   })
+  // });
+  // function buildToDo(todo){
+  //   let p = document.querySelector('p')
+  //   p.textContent = 'This is really cool!'
+  // }
